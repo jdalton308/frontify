46f82a5b680cd72b88b5dde6aee07c7dba437650
jdalton308 <jdalton308@yahoo.com>
2016-08-29 17:15:01 -0600

    Mobile draft finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 
 $(function(){
 
+	var $body = $('body');
+
 	// Menu controller
 	var Menu = (function(){
-		var 
-		function init() {
 
+		var $header = $('header');
+		var $menuTrigger = $header.find('.menu-btn');
+
+		function init() {
+			// Events
+			//---------
+			$menuTrigger.click(function(){
+				// Trigger menu to open
+				// Prevent scroll of regular page
+				$header.toggleClass('open');
+				$body.toggleClass('fixed');
+			});
 		}
+
 		return {
 			init: init
 		}
