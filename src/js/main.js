3e0a199103cb40596481f21002769f6a9f5b03b1
jdalton308 <jdalton308@yahoo.com>
2016-08-29 17:38:35 -0600

    Desktop menu working, but timing is bad

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,40 @@
 $(function(){
 
 	var $body = $('body');
+	var isMobile = (window.innerWidth < 768);
+
 
 	// Menu controller
 	var Menu = (function(){
 
 		var $header = $('header');
 		var $menuTrigger = $header.find('.menu-btn');
+		var $subMenus = $('.nav-box');
+		var $subTriggers = $subMenus.find('.nav-title');
 
 		function init() {
 			// Events
 			//---------
+
+			// Open the menu ------
 			$menuTrigger.click(function(){
-				// Trigger menu to open
 				// Prevent scroll of regular page
-				$header.toggleClass('open');
 				$body.toggleClass('fixed');
+
+				// Trigger menu to open
+				$header.toggleClass('open');
 			});
+
+			// Open sub-menus on desktop ------
+			if (!isMobile) {
+
+				$subMenus.click(function(){
+					// remove 'focus' class from other boxes
+					// add 'focus' class to this box
+					var $parentBox = $(this).closest('.nav-box');
+					$parentBox.toggleClass('focus').siblings().removeClass('focus');
+				});
+			}
 		}
 
 		return {
