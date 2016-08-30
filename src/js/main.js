c6c710889c4d615642afcc1c48e8798286edb453
jdalton308 <jdalton308@yahoo.com>
2016-08-29 21:39:39 -0600

    Finished

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,16 @@ $(function(){
 		//-----------
 		var $header = $('header');
 		var $menuTrigger = $header.find('.menu-btn');
-		var $subMenus = $('.nav-box');
-		var $subTriggers = $subMenus.find('.nav-title');
 		var $hamburgerIcon = $menuTrigger.find('.icon-menu');
+
+		var $nav = $header.find('nav');
+		var $subMenus = $nav.find('.nav-box');
+		var $subTriggers = $subMenus.find('.nav-title');
 
 		// State variables
 		//-------------------
 		var isOpen = false;
+		var subMenuOpen = false;
 
 		function animateHamburger() {
 			var stepDelay = 500;
@@ -57,33 +60,83 @@ $(function(){
 				}, 1300)
 			}
 		}
+		function toggleSubMenu() {
+			var $this = $(this);
+			var alreadyOpen = $this.hasClass('focus');
+
+			// Check if need to open or close
+			if (alreadyOpen) {
+				subMenuOpen = false;
+				closeSubMenu(true);
+			} else {
+				subMenuOpen = true;
+
+				// - Allow overflow...
+				$nav.addClass('box-in-focus');
+
+				// - If another box is open, close it...
+				closeSubMenu(false);
+
+				// - Show box
+				$this.addClass('focus');
+			}
+
+		}
+		function closeSubMenu(hideOverflow) {
+			var $openBox = $subMenus.filter('.focus');
+			console.log('$openBox in closeSubMen():');
+			console.log($openBox);
+
+			if ($openBox.length) {
+				$openBox.addClass('hide-focus');
+
+				window.setTimeout(function(){
+					$openBox.removeClass('hide-focus focus');
+
+					if (hideOverflow) {
+						$nav.removeClass('box-in-focus');
+					}
+				}, 1500);
+			}
+		}
 
 		function bindEvents() {
 
-			// Open the menu ------
+			// Toggle the menu ------
+			//-----------------------
 			$menuTrigger.click(function(){
 
 				isOpen = !isOpen;
+				var timeoutVal;
 
-				// Prevent scroll of regular page
-				$body.toggleClass('fixed', isOpen);
+				// If closing, and submenu is open...
+				if (!isMobile && !isOpen && subMenuOpen) {
+					closeSubMenu(true); // takes about 1.5sec
 
-				// Trigger menu to open
-				animateMenu();
-				// $header.toggleClass('open', isOpen);
+					timeoutVal = 1400;
+				} else {
+					timeoutVal = 0;
+				}
 
-				animateHamburger();
+				// Wait for submenu to close, if needed...
+				window.setTimeout(function(){
+					// Prevent scroll of regular page
+					$body.toggleClass('fixed', isOpen);
+
+					// Trigger menu to open
+					animateMenu();
+					// $header.toggleClass('open', isOpen);
+
+					animateHamburger();
+				}, timeoutVal);
+
 			});
 
-			// Open sub-menus on desktop ------
-			if (!isMobile) {
 
-				$subMenus.click(function(){
-					// remove 'focus' class from other boxes
-					// add 'focus' class to this box
-					var $parentBox = $(this).closest('.nav-box');
-					$parentBox.toggleClass('focus').siblings().removeClass('focus');
-				});
+			// Open sub-menus, only on desktop ------
+			//------------------------
+			if (!isMobile) {
+				$subMenus.click(toggleSubMenu);
 			}
 		}
 
